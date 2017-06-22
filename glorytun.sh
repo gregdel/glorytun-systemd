7a15c57efd9f9de3a416b557089322c5db412d9e
Grégoire Delattre <gregoire.delattre@gmail.com>
2017-06-22 21:36:04 +0200

    Setup the device manually and remote fifo

--- glorytun.sh
+++ glorytun.sh
@@ -25,56 +25,20 @@ fi
 : "${GLORYTUN_IP_PEER:=10.0.0.1}"
 : "${GLORYTUN_PORT:=5000}"
 
-statefile=/run/glorytun.fifo
-[ -p "$statefile" ] || mkfifo "$statefile"
+echo "Creating $GLORYTUN_DEV"
+ip tuntap add dev "$GLORYTUN_DEV" mode tun 2>/dev/null
+ip addr add "$GLORYTUN_IP_LOCAL" peer "$GLORYTUN_IP_PEER" dev "$GLORYTUN_DEV" 2>/dev/null
+ip link set "$GLORYTUN_DEV" txqueuelen "$GLORYTUN_TXQLEN" up 2>/dev/null
+echo "Done"
 
+echo "Connecting to $GLORYTUN_HOST on port $GLORYTUN_PORT, binding on $GLORYTUN_BIND_IPS"
 # Launch glorytun
-/usr/sbin/glorytun dev "$GLORYTUN_DEV" host "$GLORYTUN_HOST" statefile "$statefile" port "$GLORYTUN_PORT" bind-port "$GLORYTUN_PORT" mtu "$GLORYTUN_MTU" keyfile "$keyfile" bind "$GLORYTUN_BIND_IPS" &
-GTPID=$!
-
-# Catch the SIGINT / SIGTERM
-_stop() {
-	kill -TERM $GTPID
-	exit 0
-}
-trap '_stop' TERM INT QUIT
-
-# Remove the statefile on exit
-cleanup() {
-	echo "starting cleanup"
-	echo "deleting FIFO"
-	rm -f "$statefile"
-
-	echo "deleting routes"
-	ip route del "$GLORYTUN_HOST"
-	echo "routes deleted"
-	echo "cleanup done"
-}
-trap 'cleanup' EXIT
-
-while kill -0 "$GTPID"; do
-	read -r STATE DEV <"$statefile" || break
-	echo "FIFO input: $STATE $DEV"
-	case "$STATE" in
-		INITIALIZED)
-			echo "Configuring ${GLORYTUN_DEV}"
-			ip addr add "$GLORTUN_IP_LOCAL" peer "$GLORYTUN_IP_PEER" dev "$GLORYTUN_DEV"
-			ip link set "$GLORYTUN_DEV" mtu "$GLORYTUN_MTU"
-			ip link set "$GLORYTUN_DEV" txqueuelen "$GLORYTUN_TXQLEN"
-			ip link set "$GLORYTUN_DEV" up
-			echo "setting routes"
-			gateway=$(ip route get "$GLORYTUN_HOST" | grep via | awk '{ print $3 }')
-			ip route add 0.0.0.0/1 via "$GLORYTUN_IP_LOCAL"
-			ip route add 128.0.0.0/1 via "$GLORYTUN_IP_LOCAL"
-			ip route add "$GLORYTUN_HOST" via "$gateway" dev "$GLORYTUN_IF_NAME"
-			echo "routes set"
-			echo "Configuration done"
-			;;
-		STARTED)
-			echo "mud connected"
-			;;
-		STOPPED)
-			echo "mud disconnected"
-			;;
-	esac
-done
+glorytun-udp \
+	keyfile "$keyfile" \
+	host "$GLORYTUN_HOST" \
+	port "$GLORYTUN_PORT" \
+	dev "$GLORYTUN_DEV" \
+	bind-port "$GLORYTUN_PORT" \
+	mtu "$GLORYTUN_MTU" \
+	bind "$GLORYTUN_BIND_IPS" \
+	v4only
